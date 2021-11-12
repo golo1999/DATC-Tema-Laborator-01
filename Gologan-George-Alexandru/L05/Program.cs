c955a2bf4cf822975b8f82d134ae9f5c4829811b
George-Alexandru Gologan <aggologan1999@gmail.com>
2021-11-12 19:13:18 +0200

    Adding 5th lab homework

---
+++ Gologan-George-Alexandru/L05/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace L05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var count = 0;
+
+            while (count < 5)
+            {
+                Console.WriteLine("Cycle no. " + count.ToString());
+                Console.WriteLine("Date: " + DateTime.Now.ToLocalTime());
+
+                ++count;
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
