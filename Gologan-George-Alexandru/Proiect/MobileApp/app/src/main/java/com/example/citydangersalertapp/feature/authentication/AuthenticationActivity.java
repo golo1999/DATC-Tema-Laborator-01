7630980af6c8ed75ba73f85508498e46a7c16a63
George-Alexandru Gologan <aggologan1999@gmail.com>
2021-11-20 17:33:35 +0200

    Adding the starting web-app

---
+++ Gologan-George-Alexandru/Proiect/MobileApp/app/src/main/java/com/example/citydangersalertapp/feature/authentication/AuthenticationActivity.java
@@ -0,0 +1,41 @@
+package com.example.citydangersalertapp.feature.authentication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.citydangersalertapp.R;
+import com.example.citydangersalertapp.databinding.AuthenticationActivityBinding;
+
+public class AuthenticationActivity extends AppCompatActivity {
+    private AuthenticationActivityBinding binding;
+    private AuthenticationViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setVariables();
+        setLayoutVariables();
+        setFragment(new LogInFragment());
+    }
+
+    private void setFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(binding.mainContainer.getId(), fragment)
+                .commit();
+    }
+
+    private void setLayoutVariables() {
+        binding.setActivity(this);
+        binding.setViewModel(viewModel);
+    }
+
+    private void setVariables() {
+        binding = DataBindingUtil.setContentView(this, R.layout.authentication_activity);
+        viewModel = new ViewModelProvider(this).get(AuthenticationViewModel.class);
+    }
+}
--- Gologan-George-Alexandru/Proiect/Readme.txt
+++ Gologan-George-Alexandru/Proiect/Readme.txt
@@ -1,1 +1,16 @@
+Nume proiect: CityDangersAlert
 
+Membri:
+
+* George-Alexandru Gologan
+* Andrei-Daniel Ștefănoaia
+* Alexandru-Cristian Șutoi
+* Andreea-Ioana Trifa
+
+Descriere proiect:
+
+O aplicație de tipul smart city pentru monitorizarea de probleme din oraș (de genul: groapă în carosabil, coș de gunoi rasturnat, bancă distrusă, fațadă cu pericol de cădere a tencuielii, etc) și cine raporteaza probleme ce sunt rezolvate să adune puncte (și cumva să i se scadă de la impozit bani). Și această aplicație are trei componente: 
+
+* mobile app, 
+* server side (API, Firebase), 
+* dashboard (web app).
